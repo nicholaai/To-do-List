89f470e297c7701cd948883f7deb3f5a300e8216
Nicholas Pappagalo <npappagalo@gmail.com>
2015-05-05 22:38:32 -0700

    Added tasks collection, task form, & submit event listener

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,29 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1"},
-      { text: "This is task 2"},
-      { text: "This is task 3"}
-    ]
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function(event){
+      // Function called when the task form is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
   });
 }
